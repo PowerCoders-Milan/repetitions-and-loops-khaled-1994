ccddc068b83d06b0db4b3a6828a5e40ecd2efb60
khaled sidawi <khaledassa123@gmail.com>
2021-03-01 16:13:20 +0100

    added

--- 03-Multiplication-Tables/multiplication_tables.js
+++ 03-Multiplication-Tables/multiplication_tables.js
@@ -1,1 +1,24 @@
 // write your code below
+
+//In this exercise we will review a simple math concept, table of 9.
+
+//#### instructions
+//1. Write a `for loop` that will iterate from 0 to 10.
+//2. `For each` iteration of the for loop, it will multiply the number by 9.
+//3. `Log` the result (e.g. "2 * 9 = 18").
+
+//## Optional: Table of all numbers from 1 to 9
+//Write a function that gives the table of any number that you pass to it.
+
+//Have a good time, and don't get crazy with math
+var num12 = 9;
+for (x = 0 ; x <= 10; x++ ){
+    var result = x * num12
+   console.log(x + "*" + num12+ "=" + result)
+}
+function kha(num1){
+for (x = 0 ; x <= 10; x++ ){
+    var result1 = x * num1
+   console.log(x + "*" + num1+ "=" + result1)
+}}
+kha(2)
