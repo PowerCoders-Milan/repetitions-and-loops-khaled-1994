986e2c4d3f09ed5f2b351fc3cdb6a04ea08fd332
khaled sidawi <khaledassa123@gmail.com>
2021-03-01 15:08:29 +0100

    done

--- 02-Odd-Or-Even/odd_or_even.js
+++ 02-Odd-Or-Even/odd_or_even.js
@@ -5,3 +5,33 @@
   // check if the current is even or odd
 
   // report the result to the screen
+ var x = 0 ;
+ for (x= 0; x<= 20; x++){
+  if( x % 2 === 0){
+ console.log(`the number ${x} is even` + x)
+  } 
+  else{
+    console.log(`the number ${x} is odd` + x)
+  }}
+ 
+do{
+  if( x % 2 === 0){
+    console.log(`the number ${x} is evenkhaas` + x)
+     } 
+     else{
+       console.log(`the number ${x} is oddkhas` + x)
+     }
+    x++;
+}while(x <= 20);
+
+while( x <= 20){
+  
+  if( x % 2 === 0){
+    console.log(`the number ${x} is evenkha` + x)
+     } 
+     else{
+       console.log(`the number ${x} is oddkh` + x)
+     }
+    
+  x++; 
+}
