71a2af677fbaf007c50110410a7f607e93f3461f
khaled sidawi <khaledassa123@gmail.com>
2021-03-01 11:44:13 +0100

    done

--- 01-Arrays/arrays.js
+++ 01-Arrays/arrays.js
@@ -2,12 +2,20 @@
 var array = ["Banana", "Apples", "Oranges", "Blueberries"];
 
 // Access and output Oranges.
-
+var newArray = array[2];
 // Sort the array in alphabetical order.
+array.sort();
 
 // Put "Kiwi" at the end of the array.
-
+array.push("kiwi");
 // Remove "Apples" from the array.
-
+delete array[0]
 // Sort the array in reverse order, i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
+array.reverse()
+console.log(array)
+
+ 
+
+
+
 
